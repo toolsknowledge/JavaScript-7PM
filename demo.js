0859bc4bada8c523c94e16d15325e41895367bd6
samba.pendela@gmail.com <samba.pendela@gmail.com>
2021-01-27 20:06:04 +0530

    demo completed

---
+++ demo.js
@@ -0,0 +1,8 @@
+console.log("welcome to ES11");        //welcome to ES11
+console.log(10+10);                    //20
+console.log(10+"10");                  //1010
+console.log(10-"10");                  //0
+console.log(10*"10");                  //100
+console.log(10/"10");                  //1
+
+console.log( 100 + +"100" );           //200
