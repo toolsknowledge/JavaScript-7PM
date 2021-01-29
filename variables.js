af1e6ea19f8f2c4169cc647aa003b224658156ec
samba.pendela@gmail.com <samba.pendela@gmail.com>
2021-01-29 19:51:15 +0530

    variables

--- variables.js
+++ variables.js
@@ -1,3 +1,18 @@
+//var
+//let
+//cont
+
+//1) string
+//2) number
+//3) boolean
+//4) undefined
+//5) null
+//6) bigint
+//7) Symbol
+
+
+
+
 //string
 //collection of characters called as string
 //""(double quotes) , ''(single quotes) , ``(backtick operator)
@@ -44,6 +59,110 @@ console.log( 10 == "10" );              //true
 console.log( 10 === "10" );             //false
 console.log( 10 == "ten" );             //false
 console.log( 10 === "ten" );            //false
+
+
+//number
+//decimal
+//double
+//hexadecimal   ==> 0x
+//octal         ==> 0o
+//binary        ==> 0b
+var decimalNum = 100;
+var doubleNum = 100.12345;
+var hexadecimalNum = 0x123ABC;
+var octalNum = 0o123;
+var binaryNum = 0b101010;
+console.log( decimalNum,
+             doubleNum,
+             hexadecimalNum,
+             octalNum,
+             binaryNum );
+
+
+//boolean
+//true / false
+var flag = true;
+console.log( flag );        //true
+
+
+//array
+//collection of indexed and hetrogeneous Elements called as Array
+//[]
+//for() , while() , do while()
+//forEach() for...of()   for...in()
+var arr1 = [10,20,30,40,50];
+arr1.forEach(function(arg1,arg2){
+    console.log( arg1, arg2 );
+});
+
+var front_end = [`Angular`,`ReactJS`,`VueJS`];
+var databases = [`Oracle`,`MySQL`,`MongoDB`];
+front_end.forEach( (element,index)=>{
+    console.log( element, databases[index] );
+} );
+
+
+//JSON
+//Java Script Object Notation
+//Java Script "Objects"
+//"Exchange" the data over the Network
+//Objects -- {}
+//Arrays  -- []
+//data    -- key & value pairs
+//key & value separated by using ":"
+//key & value pairs separated by using ","
+var obj = {
+    sub_one : "Angular11",
+    sub_two : "NodeJS",
+    sub_three : "MongoDB"
+};
+console.log(
+    obj.sub_one,
+    obj.sub_two,
+    obj.sub_three
+);        //Angular11 NodeJS MongoDB
+
+
+//for...in     //ES6
+for(var key in obj){
+    console.log(obj[key]);
+};    //Angular11
+      //NodeJS
+      //MongoDB
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
