7dbc0aca0669cbd96e91b2e21fb1ad4b4523515b
samba.pendela@gmail.com <samba.pendela@gmail.com>
2021-02-24 20:47:10 +0530

    code

--- currying.js
+++ currying.js
@@ -1,21 +1,20 @@
 //Currying
-//Converting Tightly Coupled Business logic to loosley coupled Business logic called as Curring.
+//Converting Tightly Coupled Business Logic to Loosly Coupled Business Logic Called as Currying
 
 /*
     let add = (num1,num2)=>{
-        console.log("Addition....",num1+num2);
+        console.log("Addition...",(num1+num2));
 
         return (num1,num2)=>{
-            console.log("Subtraction....",num1-num2);
+            console.log("Subtraction...",(num1-num2));
 
-            return (num1,num2)=>{
-                console.log("Multiplication....",num1*num2);
+            return (num1,num2)=>{   
+                console.log("Multiplication....",(num1*num2));
 
                 return (num1,num2)=>{
-                    console.log("Division....",num1/num2);
+                    console.log("Division....", (num1/num2) );
                 }
             };
-
         };
 
     };
@@ -24,20 +23,21 @@
 */
 
 
-
-
 let add = (num1,num2)=>{
-    console.log("Addition....",(num1+num2));
+    console.log("Addition....",(num1+num2) );
+
     return sub;
 };
 
 let sub = (num1,num2)=>{
     console.log("Subtraction....",(num1-num2));
+
     return mul;
 };
 
-let mul = (num1,num2)=>{
-    console.log("Multiplication",(num1*num2));
+let mul = (num1,num2)=>{    
+    console.log("Multiplication....",(num1*num2));
+
     return div;
 };
 
@@ -45,9 +45,7 @@ let div = (num1,num2)=>{
     console.log("Division....",(num1/num2));
 };
 
-add(10,10)(10,10)(10,10)(10,10)
-
-
+add(10,10)(10,10)(10,10)(10,10);
 
 
 
